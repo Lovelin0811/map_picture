d06d19a1e554121600d4d895f89c1b99e691126b
sunlin <sunlin940811@gmail.com>
2026-04-09 21:02:01 +0800

    fix: update local API base to current LAN IP

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // 这里改成你绑定好的正式自定义域名
   //API_BASE: 'https://express-wnlc-239982-8-1417219022.sh.run.tcloudbase.com'
-  API_BASE: 'http://192.168.10.3:3000'
+  API_BASE: 'http://192.168.10.6:3000'
 };
