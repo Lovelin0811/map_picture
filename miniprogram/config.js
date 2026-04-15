dd71a979cf4bb53b23bf678d2624441d70537fa6
sunlin <sunlin940811@gmail.com>
2026-04-15 21:05:28 +0800

    style: refine mini program UI theme and panel layouts

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   // 这里改成你绑定好的正式自定义域名
-  //API_BASE: 'https://express-wnlc-239982-8-1417219022.sh.run.tcloudbase.com'
   // 真机调试请使用当前电脑局域网 IP
-  API_BASE: 'http://192.168.10.6:3000'
+  API_BASE: 'http://192.168.10.4:3000'
 };
