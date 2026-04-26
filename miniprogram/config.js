34ecd8000035370de75f0c3af0f0ed04733f9c27
sunlin <sunlin940811@gmail.com>
2026-04-26 19:04:34 +0800

    chore: 移除远程仓库中的IP地址，敏感信息改用config.local.js本地覆盖

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -2,13 +2,22 @@ const API_BASE_BY_ENV = {
   // 本机开发者工具
   dev: 'http://127.0.0.1:3000',
   // 真机调试（请改成当前电脑局域网 IP）
-  device: 'http://192.168.10.4:3000',
+  device: '',
   // 阿里云 ECS（备案后改为 https://lovelin.com.cn）
-  prod: 'http://47.116.214.42:3000'
+  prod: 'https://lovelin.com.cn'
 };
 
-// 只改这一行就能切环境：dev | device | prod
-const API_ENV = 'prod';
+// 本地配置覆盖（config.local.js 不入库，存放 IP 等敏感信息）
+let localConfig = {};
+try {
+  localConfig = require('./config.local.js');
+} catch (e) { /* 没有本地配置则忽略 */ }
+
+if (localConfig.API_BASE_BY_ENV) {
+  Object.assign(API_BASE_BY_ENV, localConfig.API_BASE_BY_ENV);
+}
+
+const API_ENV = localConfig.API_ENV || 'prod';
 
 module.exports = {
   API_ENV,
