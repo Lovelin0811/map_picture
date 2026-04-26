378f2a4d368dada74b94cd7705adeeecbeb4a0df
sunlin <sunlin940811@gmail.com>
2026-04-26 15:48:07 +0800

    chore: prod环境改用域名 lovelin.com.cn

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -3,8 +3,8 @@ const API_BASE_BY_ENV = {
   dev: 'http://127.0.0.1:3000',
   // 真机调试（请改成当前电脑局域网 IP）
   device: 'http://192.168.10.4:3000',
-  // 阿里云 ECS（后续配了 HTTPS 域名再改为 https://）
-  prod: 'http://47.116.214.42:3000'
+  // 阿里云 ECS（备案后改为 https://lovelin.com.cn）
+  prod: 'http://lovelin.com.cn'
 };
 
 // 只改这一行就能切环境：dev | device | prod
