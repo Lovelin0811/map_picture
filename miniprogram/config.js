00d2a49721d32108ebaf44811f70b0eaed4d843e
sunlin <sunlin940811@gmail.com>
2026-04-26 19:14:45 +0800

    chore: .workbuddy加入gitignore，防止敏感信息入库

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,8 +1,8 @@
-// 环境配置，优先读 config.local.js（不入库，含IP等敏感信息）
+// 环境配置，优先读 config.local.js（不入库，含服务器地址等敏感信息）
 // 没有 config.local.js 时默认 http://127.0.0.1:3000
 // config.local.js 示例：
 //   module.exports = {
-//     API_BASE: 'http://你的服务器地址:3000'
+//     API_BASE: 'http://后端服务器公网IP:3000'
 //   };
 
 let local = {};
